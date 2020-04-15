fccdfd8a8ae323693d159668309e0abeee629f4f
dnjoe96 <donjoedbest@gmail.com>
2020-04-15 11:51:17 +0100

    ussd

--- app/nigga2.py
+++ app/nigga2.py
@@ -447,10 +447,10 @@ def medic():
         elif text == "0":
             response = first_menu()
 
-        # elif text.split("*")[0] == '0' and len(text.split("*")) == 2 and ('00' not in text.split("*")) and ('0' not in text.split("*")):
-        #     save = text.split('*')
-        #     print(save)
-        #     response = first_menu()
+        elif text.split("*")[0] == '0' and len(text.split("*")) == 2 and ('00' not in text.split("*")) and ('0' not in text.split("*")):
+            save = text.split('*')
+            print(save)
+            response = first_menu()
 
         elif text.split("*")[0] == '0' and text.split('*')[-1] != '0' and text.split('*')[-1] != '00' and ('00' not in text.split("*")) and ('0' not in text.split("*")):
             save = text.split('*')
